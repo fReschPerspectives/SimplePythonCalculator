753f5df9239b4ee4b9e5c1564e068636dc936bba
fReschPerspectives <fresch.perspectives@outlook.com>
2025-01-31 09:45:38 -0700

    clear function added

--- SimplePythonCalculator/Calculator.py
+++ SimplePythonCalculator/Calculator.py
@@ -1,4 +1,5 @@
 import os
+from turtle import clear
 try:
     import tkinter
 except ImportError:
@@ -36,6 +37,10 @@ def entry_update(**kwargs):
 
     except ValueError:
         pass
+
+def clear():
+    result.delete(0, tkinter.END) #deletes the current value
+    entry_update(value="0.0")
 
 
 def equal_entries():
@@ -109,9 +114,9 @@ main_window.rowconfigure(6, weight=1)
 main_window.rowconfigure(7, weight=1)
 
 # Result window
-result_frame = tkinter.Frame(main_window, padx=7)
+result_frame = tkinter.Frame(main_window, padx=9)
 result_frame.grid(row=0, column=0, sticky='new')
-result = tkinter.Entry(result_frame, width = 13)
+result = tkinter.Entry(result_frame, width = 17)
 result.insert(0, '0.0')
 result.bind('<Left-Click>')
 result.grid(row=0, column=0, sticky='new')
@@ -121,14 +126,18 @@ button_frame = tkinter.Frame(main_window, padx=5)
 button_frame.grid(row=1, column=0, sticky='new')
 
 # Row 1 Buttons
-c_button = tkinter.Button(button_frame, text='C')
+c_button = tkinter.Button(button_frame, text='C', command=lambda: clear())
 ce_button = tkinter.Button(button_frame, text='CE')
 percent_button = tkinter.Button(button_frame, text='%')
 divide_button = tkinter.Button(button_frame, text='/', command=lambda: perform_operation("divide"))
 c_button.grid(row=1, column=0, sticky='nsew')
-ce_button.grid(row=1, column=1, sticky='nsew')
-percent_button.grid(row=1, column=2, sticky='nsew')
-divide_button.grid(row=1, column=3, sticky='nsew')
+c_button.config(width=2)
+ce_button.grid(row=1, column=2, sticky='nsew')
+ce_button.config(width=2)
+percent_button.grid(row=1, column=4, sticky='nsew')
+percent_button.config(width=2)
+divide_button.grid(row=1, column=6, sticky='nsew')
+divide_button.config(width=2)
 
 # Row 2 Buttons
 seven_button = tkinter.Button(button_frame, text='7', command=lambda: entry_update(value='7'))
@@ -136,9 +145,13 @@ eight_button = tkinter.Button(button_frame, text='8', command=lambda: entry_upda
 nine_button = tkinter.Button(button_frame, text='9', command=lambda: entry_update(value='9'))
 plus_button = tkinter.Button(button_frame, text='+', command=lambda: perform_operation("add"))
 seven_button.grid(row=2, column=0, sticky='nsew')
-eight_button.grid(row=2, column=1, sticky='nsew')
-nine_button.grid(row=2, column=2, sticky='nsew')
-plus_button.grid(row=2, column=3, sticky='nsew')
+seven_button.config(width=2)
+eight_button.grid(row=2, column=2, sticky='nsew')
+eight_button.config(width=2)
+nine_button.grid(row=2, column=4, sticky='nsew')
+nine_button.config(width=2)
+plus_button.grid(row=2, column=6, sticky='nsew')
+plus_button.config(width=2)
 
 # Row 3 Buttons
 four_button = tkinter.Button(button_frame, text='4', command=lambda: entry_update(value='4'))
@@ -146,9 +159,13 @@ five_button = tkinter.Button(button_frame, text='5', command=lambda: entry_updat
 six_button = tkinter.Button(button_frame, text='6', command=lambda: entry_update(value='6'))
 minus_button = tkinter.Button(button_frame, text='-', command=lambda: perform_operation("subtract"))
 four_button.grid(row=3, column=0, sticky='nsew')
-five_button.grid(row=3, column=1, sticky='nsew')
-six_button.grid(row=3, column=2, sticky='nsew')
-minus_button.grid(row=3, column=3, sticky='nsew')
+four_button.config(width=2)
+five_button.grid(row=3, column=2, sticky='nsew')
+five_button.config(width=2)
+six_button.grid(row=3, column=4, sticky='nsew')
+six_button.config(width=2)
+minus_button.grid(row=3, column=6, sticky='nsew')
+minus_button.config(width=2)
 
 # Row 4 Buttons
 one_button = tkinter.Button(button_frame, text='1', command=lambda: entry_update(value='1'))
@@ -156,9 +173,13 @@ two_button = tkinter.Button(button_frame, text='2', command=lambda: entry_update
 three_button = tkinter.Button(button_frame, text='3', command=lambda: entry_update(value='3'))
 star_button = tkinter.Button(button_frame, text='*', command=lambda: perform_operation("multiply"))
 one_button.grid(row=4, column=0, sticky='nsew')
-two_button.grid(row=4, column=1, sticky='nsew')
-three_button.grid(row=4, column=2, sticky='nsew')
-star_button.grid(row=4, column=3, sticky='nsew')
+one_button.config(width=2)
+two_button.grid(row=4, column=2, sticky='nsew')
+two_button.config(width=2)
+three_button.grid(row=4, column=4, sticky='nsew')
+three_button.config(width=2)
+star_button.grid(row=4, column=6, sticky='nsew')
+star_button.config(width=2)
 
 # Row 5 Buttons
 zero_button = tkinter.Button(button_frame, text='0')
@@ -166,8 +187,11 @@ period_button = tkinter.Button(button_frame, text='.')
 equal_button = tkinter.Button(button_frame, text='=', command=lambda: equals())
 
 zero_button.grid(row=5, column=0, sticky='nsew')
-period_button.grid(row=5, column=1, sticky='nsew')
-equal_button.grid(row=5, column=2, sticky='nsew', columnspan=2)
+zero_button.config(width=2)
+period_button.grid(row=5, column=2, sticky='nsew')
+period_button.config(width=2)
+equal_button.grid(row=5, column=4, sticky='nsew', columnspan=3)
+equal_button.config(width=8)
 
 
 main_window.update()
